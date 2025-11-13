72ab336ff29004ccd2929ba1deccc95cd06201e7
Mats Byrkjeland <git@byrkje.land>
2025-11-13 18:29:17 +0100

    Remove try/catch so build fails if data fetching fails
    This empty catch block made the build script continue so that the app
    was deployed without any stair data.

--- fetchData.js
+++ fetchData.js
@@ -6,10 +6,9 @@ import { join } from "node:path";
  * GeoJSON and saves it to public/stairs.geojson.
  */
 async function fetchData() {
-  try {
-    const overpassUrl = "https://overpass-api.de/api/interpreter";
+  const overpassUrl = "https://overpass-api.de/api/interpreter";
 
-    const query = `
+  const query = `
       [out:json][timeout:300];
       area["ISO3166-1:alpha2"="NO"]->.a;
       (
@@ -20,71 +19,68 @@ async function fetchData() {
       out skel qt;
     `;
 
-    console.log("Querying Overpass API...");
-    const start = Date.now();
+  console.log("Querying Overpass API...");
+  const start = Date.now();
 
-    const response = await fetch(overpassUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `data=${encodeURIComponent(query)}`,
-    });
+  const response = await fetch(overpassUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: `data=${encodeURIComponent(query)}`,
+  });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    console.log(
-      "Got successful response from Overpass API after",
-      (Date.now() - start) / 1000,
-      "seconds",
-    );
-
-    const data = await response.json();
-
-    const nodes = data.elements
-      .filter((element) => element.type === "node")
-      .reduce((acc, node) => {
-        acc[node.id] = node;
-        return acc;
-      }, {});
-
-    const features = data.elements
-      .filter((element) => element.type === "way")
-      .map((element) => ({
-        id: element.id,
-        type: "Feature",
-        geometry: {
-          type: "LineString",
-          coordinates: element.nodes.map((nodeId) => [
-            nodes[nodeId].lon,
-            nodes[nodeId].lat,
-          ]),
-        },
-        properties: {
-          id: element.nodes[0],
-          name: element.tags.name,
-          step_count: element.tags.step_count
-            ? Number(element.tags.step_count)
-            : undefined,
-        },
-      }));
-
-    console.log("Found", features.length, "features");
-
-    const featureCollection = {
-      type: "FeatureCollection",
-      features,
-    };
-
-    console.log("Writing to stairs.geojson...");
-
-    await writeFile(
-      join("public", "stairs.geojson"),
-      JSON.stringify(featureCollection),
-    );
-  } catch (error) {
-    console.error(error);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
+
+  console.log(
+    "Got successful response from Overpass API after",
+    (Date.now() - start) / 1000,
+    "seconds",
+  );
+
+  const data = await response.json();
+
+  const nodes = data.elements
+    .filter((element) => element.type === "node")
+    .reduce((acc, node) => {
+      acc[node.id] = node;
+      return acc;
+    }, {});
+
+  const features = data.elements
+    .filter((element) => element.type === "way")
+    .map((element) => ({
+      id: element.id,
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: element.nodes.map((nodeId) => [
+          nodes[nodeId].lon,
+          nodes[nodeId].lat,
+        ]),
+      },
+      properties: {
+        id: element.nodes[0],
+        name: element.tags.name,
+        step_count: element.tags.step_count
+          ? Number(element.tags.step_count)
+          : undefined,
+      },
+    }));
+
+  console.log("Found", features.length, "features");
+
+  const featureCollection = {
+    type: "FeatureCollection",
+    features,
+  };
+
+  console.log("Writing to stairs.geojson...");
+
+  await writeFile(
+    join("public", "stairs.geojson"),
+    JSON.stringify(featureCollection),
+  );
 }
 
 async function main() {
